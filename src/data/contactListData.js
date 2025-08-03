41a1f97e40f6c11a821ff5a12a5089209c978845
Agustinbaigo <agustinbaigorria97@gmail.com~>
2025-08-03 03:43:40 -0300

    update 1

--- src/data/contactListData.js
+++ src/data/contactListData.js
@@ -2,20 +2,19 @@ const contactListData = [
     {
         id: 1,
         name: "Pepito",
-        avatar: "https://i.pinimg.com/736x/9d/6b/9d/9d6b9db2dcb0526a09b89fb35d075c72.jpg",
-        online: true,
+        avatar: "https://images.squarespace-cdn.com/content/v1/5d77a7f8ad30356d21445262/1695001086188-Y7J4ZAPMINL2KJBCYOPZ/ideas-foto-de-perfil.jpg?format=1000w",
         chat: [
             {
-                message: "hola", hour: "06:54 p. m.", sender:"me"
+                message: "hola", hour: "10:04 p. m.", sender: "me"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"other"
+                message: "hola", hour: "10:05 a. m.", sender: "other"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"me"
+                message: "todo tranqui?", hour: "10:20 a. m.", sender: "me"
             },
             {
-                message: "hola", hour: "06:54 p. m.", sender:"other"
+                message: "todo de 10, re colgue pero bueno xdd", hour: "06:54 p. m.", sender: "other"
             }
         ]
     },
@@ -23,71 +22,68 @@ const contactListData = [
         id: 2,
         name: "Juanito",
         avatar: "https://i.pinimg.com/736x/9d/6b/9d/9d6b9db2dcb0526a09b89fb35d075c72.jpg",
-        online: true,
         chat: [
             {
-                message: "hola", hour: "06:54 p. m.", sender:"me"
+                message: "hola juanito", hour: "11:01 p. m.", sender: "me"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"other"
+                message: "hola wachinnn", hour: "11:04 a. m.", sender: "other"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"me"
+                message: "q hacias", hour: "11:06 a. m.", sender: "me"
             },
             {
-                message: "hola", hour: "06:54 p. m.", sender:"other"
+                message: "nada aca haciendome una sopa", hour: "12:10 p. m.", sender: "other"
             }
         ]
     },
-    {
-        id: 3,
-        name: "Cacho",
-        avatar: "https://i.pinimg.com/736x/9d/6b/9d/9d6b9db2dcb0526a09b89fb35d075c72.jpg",
-        online: false,
-        chat: [
 
-        ]
-    },
     {
         id: 4,
         name: "Maga",
-        avatar: "https://i.pinimg.com/736x/9d/6b/9d/9d6b9db2dcb0526a09b89fb35d075c72.jpg",
-        online: true,
+        avatar: "https://images.squarespace-cdn.com/content/v1/5d77a7f8ad30356d21445262/1694999941191-ZWZOY86NCKNYJ7XFULK1/fotos-de-perfil-aesthetic-2.jpg?format=750w",
         chat: [
             {
-                message: "hola", hour: "06:54 p. m.", sender:"me"
+                message: "magaaa", hour: "06:54 p. m.", sender: "me"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"other"
+                message: "q paso", hour: "07:02 p. m.", sender: "other"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"me"
+                message: "necesito un favor urgente", hour: "07:15 p. m.", sender: "me"
             },
             {
-                message: "hola", hour: "06:54 p. m.", sender:"other"
+                message: "q onda re flashaste confianza ni q fueramos amigos tomatela gato vigote de leche q te pensa ah na que ve, decime ", hour: "07:33 p. m.", sender: "other"
             }
         ]
     },
     {
         id: 5,
         name: "Sofia",
-        avatar: "https://i.pinimg.com/736x/9d/6b/9d/9d6b9db2dcb0526a09b89fb35d075c72.jpg",
-        online: false,
+        avatar: "https://images.squarespace-cdn.com/content/v1/5d77a7f8ad30356d21445262/1694999941257-XTNIAK3D8O3AC92VATDF/fotos-de-perfil-aesthetic-1.jpg?format=750w",
         chat: [
             {
-                message: "hola", hour: "06:54 p. m.", sender:"me"
+                message: "hola", hour: "02:00 a. m.", sender: "me"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"other"
+                message: "hola", hour: "02:04 a. m.", sender: "other"
             },
             {
-                message: "hola", hour: "10:04 a. m.", sender:"me"
+                message: "nada ya hasta me dio fiaca pensar en chats", hour: "02:04 a. m.", sender: "me"
             },
             {
-                message: "hola", hour: "06:54 p. m.", sender:"other"
+                message: "y si hermano mira la hora", hour: "02:11 a. m.", sender: "other"
             }
         ]
     },
+    {
+        id: 3,
+        name: "Cacho",
+        avatar: "https://images.squarespace-cdn.com/content/v1/5d77a7f8ad30356d21445262/1695000049347-TEVVEON67JR6NEOQC0FB/fotos-de-perfil-bonitas.jpg?format=750w",
+        chat: [
+
+        ]
+    }
 
 ]
 export default contactListData;
